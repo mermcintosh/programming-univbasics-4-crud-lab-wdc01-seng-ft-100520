dbeb89326cd5c07643b293720f5e69a4194d54c7
Meredith McIntosh <meredith@mcintosh.io>
2020-09-15 16:48:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,15 +18,6 @@ def add_element_to_start_of_array(array, element)
 end
 add_element_to_start_of_array(["apple","banana","peach","blueberry"], "orange")
 
-
-
-
-
-
-
-
-
-
 # def remove_element_from_end_of_array(array)
   
 # end
