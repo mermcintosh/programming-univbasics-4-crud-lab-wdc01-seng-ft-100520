15acf0d07ec1b4c39c8d7f9be56a6f95c610472c
Meredith McIntosh <meredith@mcintosh.io>
2020-09-15 17:24:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,6 +43,6 @@ end
 retrieve_last_element_from_array(["apple","banana","peach","blueberry"])
 
 def update_element_from_index(array, index_number, element)
-array[2] = "orange"
+array[2]
 end
 update_element_from_index(["apple","banana","peach","blueberry"], [2], "orange")
